c5f963c078af62b9402fbd36bf454e78655cf369
Ricardo Faria <ricardofariah912@gmail.com>
2024-08-06 22:39:19 +0200

    Ending starting preparations.
    - Header updated with 2 functions.
    - Bitset getter method in progress.
    - toBitsetStr method in progress.

--- BitwiseParser/Parser.h
+++ BitwiseParser/Parser.h
@@ -1,1 +1,25 @@
 #pragma once
+#include <bitset>
+
+class Parser {
+public:
+		template<int Size>
+		static std::bitset<Size> getBitset(const std::string& input)
+		{
+			int content = std::stoi(input);
+			int bits = 0;
+			bool isNegative = false;
+
+			//TODO: bitset getter.
+			
+		}
+
+		static std::string bitSetStr(const std::string& input)
+		{
+			
+				
+
+			
+			
+		}
+};
